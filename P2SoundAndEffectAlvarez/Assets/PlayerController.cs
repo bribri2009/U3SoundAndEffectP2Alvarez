82b275ec6c31276fbb53bbc39beacf4380d455c6
bribri2009 <735857@student.dusd.net>
2025-05-02 09:57:50 -0700

    WORKED
    starting lesson 3.4

--- P2SoundAndEffectAlvarez/Assets/PlayerController.cs
+++ P2SoundAndEffectAlvarez/Assets/PlayerController.cs
@@ -6,25 +6,28 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private Animator playerAnim;
     public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround = true;
-    public bool gameOver;
+    public bool gameOver = false;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver )
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
         }
     }
     private void OnCollisionEnter(Collision collision)
@@ -38,6 +41,9 @@ public class PlayerController : MonoBehaviour
         } else if(collision.gameObject.CompareTag("Obstacle"))
         {
             Debug.Log("Game Over!");
+            gameOver = true;
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
         }
     }
 }
